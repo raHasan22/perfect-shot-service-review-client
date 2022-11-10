21e5c3cb300845c954944ef34d6c971974fb15a6
raHasan22 <r.ahmad.hasan.r@gmail.com>
2022-11-10 20:52:31 +0600

    log in page edited and firebase added

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -7,26 +7,26 @@ const Login = () => {
             <div className="hero min-h-screen bg-base-200">
                 <div className="hero-content w-4/5 flex-col lg:flex-row-reverse">
                 <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                    <div className="card-body">
+                    <form className="card-body">
                         <div className="form-control">
                         <label className="label">
                             <span className="label-text">Email</span>
                         </label>
-                        <input type="text" placeholder="email" className="input input-bordered" />
+                        <input type="email" name='email' placeholder="email" className="input input-bordered" />
                         </div>
                         <div className="form-control">
                         <label className="label">
                             <span className="label-text">Password</span>
                         </label>
-                        <input type="text" placeholder="password" className="input input-bordered" />
+                        <input type="password" name='password' placeholder="password" className="input input-bordered" />
                         <label className="label">
                             <p>Don't have a account <Link to='/signup' className="label-text-alt link link-hover">Sign Up</Link></p>
                         </label>
                         </div>
                         <div className="form-control mt-6">
-                        <button className="btn btn-primary">Login</button>
+                        <input className="btn btn-primary font-bold" type="submit" value="Login" />
                         </div>
-                    </div>
+                    </form>
                     </div>
                     <div className="text-center lg:text-left">
                     <h1 className="text-5xl font-bold">Login now!</h1>
--- src/pages/signUp/SignUp.js
+++ src/pages/signUp/SignUp.js
@@ -10,32 +10,32 @@ const SignUp = () => {
                 <h1 className="text-5xl font-bold">Register !!</h1>
                 </div>
                 <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                <div className="card-body">
+                <form className="card-body">
                     <div className="form-control">
                     <label className="label">
                         <span className="label-text">Name</span>
                     </label>
-                    <input type="text" placeholder="Your Name" className="input input-bordered" />
+                    <input type="text" name='name' placeholder="Your Name" className="input input-bordered" />
                     </div>
                     <div className="form-control">
                     <label className="label">
                         <span className="label-text">Email</span>
                     </label>
-                    <input type="email" placeholder="email" className="input input-bordered" />
+                    <input type="email" name='email' placeholder="email" className="input input-bordered" />
                     </div>
                     <div className="form-control">
                     <label className="label">
                         <span className="label-text">Password</span>
                     </label>
-                    <input type="password" placeholder="password" className="input input-bordered" />
+                    <input type="password" name='password' placeholder="password" className="input input-bordered" />
                     <label className="label">
                         <p>Already registered ? <Link to='/login' className="label-text-alt link link-hover">Sign in</Link></p>
                     </label>
                     </div>
                     <div className="form-control mt-6">
-                    <button className="btn btn-primary">Login</button>
+                        <input className="btn btn-primary font-bold" type="submit" value="Register" />
                     </div>
-                </div>
+                </form>
                 </div>
             </div>
             </div>
